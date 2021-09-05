50822dcc473eb2cdb0f637f81d22f267df9d7f1d
klemenkerin <klemen.kerin01@gmail.com>
2021-09-05 21:58:07 +0200

    popravek faq container

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -4,31 +4,33 @@ function FAQ() {
 
     return (
         <div className="faq" id="faq">
-            <h2 class="text-center width-50">FAQ</h2>
-            <div class="flex-container margin-top-50">
-                <div class="question">
-                    <h3>What are Lazy Hippos?</h3>
-                    <p>Lazy Hippos is a collection of 10.000 algorithmically generated Hippos with hand drawn attributes. You can get your Hippo in the form of a nft on Solana blockchain.</p>
-                </div>
-                <div class="question">
-                    <h3>When is the mint date?</h3>
-                    <p>We are planning to first create a presale for our most loyal community members. The main launch will happen soon after the presale. We are anticipating both launches to happend in the first half of September.</p>
-                </div>
-                <div class="question">
-                    <h3>Which Solana wallets can I use?</h3>
-                    <p>We are going to support Phantom wallet. If there is a big demand for other wallets, we can add those too.</p>
-                </div>
-                <div class="question">
-                    <h3>Who are you?</h3>
-                    <p>We are a team of three friends who love art and are passionate about crpyto. That's why we decided to create our own NFT. We are totaly commited to this project and our community to make Lazy Hippos the best nft on Solana blockchain ❤️</p>
-                </div>
-                <div class="question">
-                    <h3>How much will it cost to mint a Hippo?</h3>
-                    <p>Presale price will be only 1 SOL, so make sure you join our discord to be able to mint at presale. Main launch price will be 3 SOL per Hippo.</p>
-                </div>
-                <div class="question">
-                    <h3>Is there a limit on how many we can get?</h3>
-                    <p>There is no such limit, you can mint as many as you want if you're fast enough :)</p>
+            <div className="container">
+                <h2 class="text-center width-50">FAQ</h2>
+                <div class="flex-container margin-top-50">
+                    <div class="question">
+                        <h3>What are Lazy Hippos?</h3>
+                        <p>Lazy Hippos is a collection of 10.000 algorithmically generated Hippos with hand drawn attributes. You can get your Hippo in the form of a nft on Solana blockchain.</p>
+                    </div>
+                    <div class="question">
+                        <h3>When is the mint date?</h3>
+                        <p>We are planning to first create a presale for our most loyal community members. The main launch will happen soon after the presale. We are anticipating both launches to happen in the first half of September.</p>
+                    </div>
+                    <div class="question">
+                        <h3>Which Solana wallets can I use?</h3>
+                        <p>We are going to support Phantom wallet. If there is a big demand for other wallets, we can add those too.</p>
+                    </div>
+                    <div class="question">
+                        <h3>Who are you?</h3>
+                        <p>We are a team of three friends who love art and are passionate about crpyto. That's why we decided to create our own NFT. We are totaly commited to this project and our community to make Lazy Hippos the best nft on Solana blockchain ❤️</p>
+                    </div>
+                    <div class="question">
+                        <h3>How much will it cost to mint a Hippo?</h3>
+                        <p>Presale price will be only 1 SOL, so make sure you join our discord to be able to mint at presale. Main launch price will be 3 SOL per Hippo.</p>
+                    </div>
+                    <div class="question">
+                        <h3>Is there a limit on how many we can get?</h3>
+                        <p>There is no such limit, you can mint as many as you want if you're fast enough :)</p>
+                    </div>
                 </div>
             </div>
 
